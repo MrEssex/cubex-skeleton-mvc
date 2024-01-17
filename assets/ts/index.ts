dff92900fd61da4c358217ac0d170d2bc431da38
Kyle Essex <kylegavinessex@hotmail.com>
2024-01-17 14:41:42 +0000

    Add Type definition

--- assets/ts/index.ts
+++ assets/ts/index.ts
@@ -1,11 +1,7 @@
-// @ts-ignore
-import { PageletEvent, Pagelets } from "@packaged-ui/pagelets";
+import { PageletEvent, Pagelets } from '@packaged-ui/pagelets';
 
-console.log('Hello World');
+Pagelets.init({ 'defaultTarget': 'main', 'selector': 'a:not(.nar)' });
 
-Pagelets.init({ "defaultTarget": "main", "selector": "a:not(.nar)" });
-
-//@ts-ignore
 document.addEventListener(Pagelets.events.PREPARE, (e: PageletEvent) => {
   e.detail.request.withCredentials = true;
 });
